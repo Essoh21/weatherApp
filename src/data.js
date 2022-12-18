b53e3df36350bfc10c1f990cc42e44f4bd2cef91
essohanam <essohanam.alou@imsp-uac.org>
2022-12-18 23:14:54 +0000

    full html and css with api temp fetch

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@
 const openWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const appId = '&APPID=f9a888b731855bb8fb61a39b61e95564';
 const dataUnits = '&units=metric';
-async function getWetherDataFromLocation(locationName) {
+async function getWeatherDataFromLocation(locationName) {
     locationName = `${locationName}`;
     const locationDataUrl = openWeatherUrl + locationName + appId + dataUnits;
     const weatherPromise = await (fetch(locationDataUrl));
@@ -10,4 +10,9 @@ async function getWetherDataFromLocation(locationName) {
     return weatherData;
 }
 
-export default getWetherDataFromLocation;
+async function getTemperatureOf(location) {
+    const locationDataObject = await getWeatherDataFromLocation(location);
+    console.log(locationDataObject);
+}
+
+export default getWeatherDataFromLocation;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
 import html from './index.html';
 import style from './style.css';
-import getWetherDataFromLocation from './data';
+import getWeatherDataFromLocation from './data';
 
-const resquestedData = getWetherDataFromLocation('kara').then(data => console.log(data));
+let locationToSearch = document.querySelector('#location-collecter');
+let locationName = 'Lomé';
+let locationNameContainer = document.querySelector('.location-name');
+const tempContainer = document.querySelector('.temp');
+const searchIcon = document.querySelector('.search-icon');
+//const resquestedData = getWeatherDataFromLocation('Lomé').then(data => console.log(data.main['temp']));
+async function getTemperatureOf(location) {
+    const locationDataObject = await getWeatherDataFromLocation(location);
+    if (!(locationToSearch.value === '')) {
+        locationName = locationToSearch.value.toUpperCase();
+        locationNameContainer.innerHTML = locationName;
+    } else {
+        locationNameContainer.innerHTML = locationName
+    }
+    const locationTemperature = await locationDataObject.main['temp'];
+    tempContainer.innerHTML = `${locationTemperature} C`;
+}
+
+
+getTemperatureOf('lomé');
+function updateLocationName() {
+    locationNameContainer.innerHTML = locationToSearch.value.toUpperCase();
+}
+
+function clearInput(inputToclear) {
+    inputToclear.value = '';
+}
+
+searchIcon.addEventListener('click', updateLocationName);
+locationToSearch.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        updateLocationName();
+        clearInput(locationToSearch);
+    }
+})
